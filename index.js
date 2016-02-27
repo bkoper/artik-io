5519d7cbfb693ffbbd9955002b1eb30b16053bfc
Bartlomiej Koper <bkimac@iMac-bk.local>
2016-02-27 22:23:14 +0100

    testing with package

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var artikGpio = require("./build/artik-gpio");
 var artikIo = require("./build/artik-io");
 
 module.exports = {
-    artik-gpio: artikGpio
-    artik-io: artikIo
+    "artik-gpio": artikGpio,
+    "artik-io": artikIo
 };
