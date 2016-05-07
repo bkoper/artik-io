17c673982e44567b0e1742132bb28e2c3b3b23d1
Bartlomiej Koper <bkoper@bkoper.local>
2016-05-07 23:03:22 +0200

    simplified export

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 module.exports = {
-    lib: {
-        Gpio: require("./dist/artik-gpio").default,
-        GpioIO: require("./dist/artik-io").default
-    },
-    devices: {
-        Device: require("./dist/devices/device").default,
-        led: require("./dist/led").default,
-        rainDetector: require("./dist/devices/rainDetector").default,
-        soilHumid: require("./dist/devices/soilHumid").default,
-        switcher: require("./dist/devices/switcher_srd-05vdc-sl-c").default,
-        waterFlow: require("./dist/devices/waterFlowSensor").default
-    }
+    Gpio: require("./dist/artik-gpio").default,
+    GpioIO: require("./dist/artik-io").default
+    Device: require("./dist/devices/device").default,
+    led: require("./dist/led").default,
+    rainDetector: require("./dist/devices/rainDetector").default,
+    soilHumid: require("./dist/devices/soilHumid").default,
+    switcher: require("./dist/devices/switcher_srd-05vdc-sl-c").default,
+    waterFlow: require("./dist/devices/waterFlowSensor").default
 };
