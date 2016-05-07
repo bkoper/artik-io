d9864447ef9e132b40336facab6a4558589bc2ee
Bartlomiej Koper <bkoper@bkoper.local>
2016-05-07 23:19:59 +0200

    dist arch changing to flat

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = {
     Gpio: require("./dist/artik-gpio").default,
-    GpioIO: require("./dist/artik-io").default
-    Device: require("./dist/devices/device").default,
+    GpioIO: require("./dist/artik-io").default,
+    Device: require("./dist/device").default,
     led: require("./dist/led").default,
-    rainDetector: require("./dist/devices/rainDetector").default,
-    soilHumid: require("./dist/devices/soilHumid").default,
-    switcher: require("./dist/devices/switcher_srd-05vdc-sl-c").default,
-    waterFlow: require("./dist/devices/waterFlowSensor").default
+    rainDetector: require("./dist/rainDetector").default,
+    soilHumid: require("./dist/soilHumid").default,
+    switcher: require("./dist/switcher_srd-05vdc-sl-c").default,
+    waterFlow: require("./dist/waterFlowSensor").default
 };
