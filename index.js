80a5ad12598a58dffbd931483760d66dca1492e0
Bartlomiej Koper <bkoper@bkoper.local>
2016-05-07 23:58:43 +0200

    limiting export only to lib

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 module.exports = {
     Gpio: require("./dist/artik-gpio").default,
-    GpioIO: require("./dist/artik-io").default,
-    Device: require("./dist/device").default,
-    led: require("./dist/led").default,
-    rainDetector: require("./dist/rainDetector").default,
-    soilHumid: require("./dist/soilHumid").default,
-    switcher: require("./dist/switcher_srd-05vdc-sl-c").default,
-    waterFlow: require("./dist/waterFlowSensor").default
+    GpioIO: require("./dist/artik-io").default
 };
